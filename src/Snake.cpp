e72d9295ee57e6816b617f7696ab924f6613fb6e
Nikhil S. Hore <niknsh5@gmail.com>
2021-08-26 17:42:43 +0530

    minor changes

--- src/Snake.cpp
+++ src/Snake.cpp
@@ -22,10 +22,6 @@ void Snake::move_snake(){
         case 'l': pos.X-= vel; break; // decrease x coordinate to move left
         case 'r': pos.X+= vel; break; // increase x coordinate to move right
     }
-    // if (pos.X>width-2) pos.X=1;
-    // if (pos.X<1) pos.X= width-2;
-    // if (pos.Y>height-2) pos.Y=1;
-    // if (pos.Y<1) pos.Y= height-2;
 
     body.push_back(pos);
     if (body.size()>len) body.erase(body.begin());
